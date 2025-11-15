e1ce21e1b4d1967633d31279ba9d1eafbae3a355
ISHITA ROY <royishita016@gmail.com>
2025-11-15 18:21:43 +0530

    Mini Projects, Experience and Connect sections added. Details r to be added.

---
+++ src/components/Connect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Connect = () => {
+  return (
+    <div id='connect' className='flex-grow bg-darkSecondary w-full md:w-3/4 max-w-[95%] mx-auto mt-3 sm:mt-6 p-4 sm:p-6 rounded-lg flex flex-col md:flex-row justify-start items-center gap-4 sm:gap-8'>
+      Connect with me
+    </div>
+  )
+}
+
+export default Connect
---
+++ src/components/MiniProjects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SkillsTechMarquee from './SkillsTechMarquee'
+
+const MiniProjects = () => {
+    return (
+        <div id='miniprojects' className='flex-grow bg-darkSecondary w-full md:w-3/4 max-w-[95%] mx-auto mt-3 sm:mt-6 p-4 sm:p-6 rounded-lg flex flex-col md:flex-row justify-start items-center gap-4 sm:gap-8'>
+            Mini Project
+        </div>
+    )
+}
+
+export default MiniProjects
